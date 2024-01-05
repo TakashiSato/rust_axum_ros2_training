891368f5aabe6bb5a01655b5499860c73eb0d368
Takashi Sato <t.sato17123@gmail.com>
2024-01-05 12:25:27 +0900

    impl Gateway

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,15 @@ use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = r2r::Context::create()?;
-    let node = r2r::Node::create(ctx, "rust_axum_ros2_training_node", "")?;
-    let arc_node = Arc::new(Mutex::new(node));
+    let gateway = Gateway::new()?;
+    let arc_gateway = Arc::new(Mutex::new(gateway));
 
     tracing_subscriber::fmt::init();
     let app = Router::new()
         .route("/", get(root))
         .route("/hello/:name", get(json_hello))
         .route("/user", post(create_user))
-        .with_state(arc_node.clone());
+        .with_state(arc_gateway.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -32,6 +31,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
+}
+
+struct Gateway {
+    node: r2r::Node,
+    user_pub: r2r::Publisher<std_msgs::msg::String>,
+}
+
+impl Gateway {
+    fn new() -> Result<Gateway, Box<dyn std::error::Error>> {
+        let ctx = r2r::Context::create()?;
+        let mut node = r2r::Node::create(ctx, "rust_axum_ros2_training_node", "")?;
+
+        let user_pub =
+            node.create_publisher::<std_msgs::msg::String>("user", QosProfile::default())?;
+
+        Ok(Gateway {
+            node: node,
+            user_pub: user_pub,
+        })
+    }
+
+    fn publish_user(&self, user: User) {
+        let msg = r2r::std_msgs::msg::String {
+            data: user.username.clone(),
+        };
+        self.user_pub.publish(&msg).unwrap();
+    }
 }
 
 async fn root() -> &'static str {
@@ -58,7 +84,7 @@ struct User {
 }
 
 async fn create_user(
-    State(arc_node): State<Arc<Mutex<r2r::Node>>>,
+    State(arc_gateway): State<Arc<Mutex<Gateway>>>,
     Json(payload): Json<CreateUser>,
 ) -> impl IntoResponse {
     let user = User {
@@ -66,16 +92,8 @@ async fn create_user(
         username: payload.username,
     };
 
-    let publisher = arc_node
-        .lock()
-        .unwrap()
-        .create_publisher::<std_msgs::msg::String>("user", QosProfile::default())
-        .unwrap();
-
-    let msg = r2r::std_msgs::msg::String {
-        data: user.username.clone(),
-    };
-    publisher.publish(&msg).unwrap();
+    let gateway = arc_gateway.lock().unwrap();
+    gateway.publish_user(user.clone());
 
     (StatusCode::CREATED, Json(user))
 }
